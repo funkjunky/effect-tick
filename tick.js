0ecd86dbcbf4b67922645ae2b79cb4b1fc2bc2e6
Jason McCarrell <jason.abz@gmail.com>
2018-01-28 15:20:51 -0500

    converted effect to action.

--- tick.js
+++ tick.js
@@ -1,58 +1,54 @@
 const GeneratorFunction = function*(){}.constructor;
 
-export const TYPE__TICK = '__INTERVAL_TICK__';
+export const TICK_ADD = 'TICK_ADD';
+export const TICK_PAUSE = 'TICK_PAUSE';
+export const TICK_RESUME = 'TICK_RESUME';
 
-//Description, The thing you yield
-export const tick = fnc => ({
-    type: TYPE__TICK,
-    payload: {
-        fnc
-    }
+export const addTick = actionCreator => ({
+    type: TICK_ADD,
+    actionCreator
 });
 
-export const generateTickFunctions = () => {
-    //Processor adds subscribers, while the middleware runs them
+export const pauseTicks = () => ({
+    type: TICK_PAUSE
+});
+
+export const resumeTicks = () => ({
+    type: TICK_RESUME
+});
+
+//Note: You have to start the interval yourself, by dispatching resumeTicks
+export const tickMiddleware = store => {
+    let interval;
     let subscribers = [];
-    //The processor has access to the static effectProcessor
-    //so I make it available for the middleware when it processes the generator passed to tick
-    let effectProcessor;
-
-    const processor = (effect, { effectGeneratorProcessor }) => {
-        effectProcessor = effectGeneratorProcessor;
-
-        if(effect.payload.fnc instanceof GeneratorFunction) {
-            return new Promise(resolve => subscribers.push([effect.payload.fnc, resolve]));
-        } else {
-            return Promise.reject('tick only takes a generator as a callback');
-        }
-    };
-
-    const middleware = (interval = 1000) => store => {
-        let lastTime = Date.now();
-        setInterval(() => {
-            const dt = Date.now() - lastTime;
-            lastTime = Date.now();
-            subscribers.forEach(([fnc, resolver]) => {
-                if(fnc instanceof GeneratorFunction) {
-                    const res = effectProcessor(fnc(dt), { dispatch: store.dispatch });
-                    res.then(isComplete => {
-                        if(isComplete) {
-                            subscribers = subscribers.filter(([fnc, rr]) => rr !== resolver);
-                            resolver();
-                        }
-                    });
-                } else {
-                    const isComplete = fnc(dt);
-                    if(isComplete) {
-                        subscribers = subscribers.filter(([fnc, rr]) => rr !== resolver);
-                        resolver();
-                    }
+    function startInterval() {
+        interval = setInterval(() => {
+            subscribers.forEach(async tickArgs => {
+                const [actionCreator, lastTime, resolver] = tickArgs;
+                const dt = Date.now() - lastTime;
+                tickArgs[1] = Date.now();
+                const result = await store.dispatch(actionCreator(dt));
+                if(result) {
+                    subscribers = subscribers.filter(([_, __, rr]) => rr !== resolver);
+                    resolver();
                 }
             });
-        }, interval);
+        }, 1);
+    }
 
-        return next => action => next(action);
+    function clearInterval() {
+        clearInterval(interval);
+    }
+
+    return next => action => {
+        if(action.type === TICK_ADD) {
+            return new Promise(resolve => subscribers.push([action.actionCreator, Date.now(), resolve]));
+        } else if(action.type === TICK_PAUSE) {
+            clearInterval();
+        } else if(action.type === TICK_RESUME) {
+            startInterval();
+        } else {
+            return next(action);
+        }
     };
-
-    return { processor, middleware };
 };
