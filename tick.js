008e1421853ba296c3303be8722553d79418ca60
Jason McCarrell <jason.abz@gmail.com>
2020-09-30 10:56:41 -0400

    Updated to accept a interval registration

--- tick.js
+++ tick.js
@@ -4,9 +4,10 @@ export const TICK_ADD = 'TICK_ADD';
 export const TICK_PAUSE = 'TICK_PAUSE';
 export const TICK_RESUME = 'TICK_RESUME';
 
-export const addTick = actionCreator => ({
+export const addTick = (actionCreator, curTime = Date.now()) => ({
   type: TICK_ADD,
-  actionCreator
+  actionCreator,
+  curTime,
 });
 
 export const pauseTicks = () => ({
@@ -17,34 +18,37 @@ export const resumeTicks = () => ({
   type: TICK_RESUME
 });
 
+const defaultInterval = cb => {
+  const h = setInterval(() => cb(Date.now()), 100);
+  return () => window.clearInterval(h);
+};
+
 //Note: You have to start the interval yourself, by dispatching resumeTicks
-export const tickMiddleware = store => {
-  let interval;
+export const tickMiddleware = (registerInterval = defaultInterval) => store => {
+  let clearInterval;
   let subscribers = [];
+
   function startInterval() {
-    interval = setInterval(() => {
+    clearInterval = registerInterval(curTime => {
       subscribers.forEach(async tickArgs => {
         const [actionCreator, lastTime, resolver] = tickArgs;
-        const dt = Date.now() - lastTime;
-        tickArgs[1] = Date.now();
+        const dt = curTime - lastTime;
+        tickArgs[1] = curTime;
         const result = await store.dispatch(actionCreator(dt));
         if(result) {
           subscribers = subscribers.filter(([_, __, rr]) => rr !== resolver);
           resolver();
         }
       });
-    }, 1);
-  }
-
-  function clearInterval() {
-    window.clearInterval(interval);
+    });
   }
 
   return next => action => {
     if(action.type === TICK_ADD) {
-      return new Promise(resolve => subscribers.push([action.actionCreator, Date.now(), resolve]));
+      return new Promise(resolve => subscribers.push([action.actionCreator, action.curTime || Date.now(), resolve]));
     } else if(action.type === TICK_PAUSE) {
-      clearInterval();
+      clearInterval && clearInterval();
+      clearInterval = null;
     } else if(action.type === TICK_RESUME) {
       startInterval();
     } else {
